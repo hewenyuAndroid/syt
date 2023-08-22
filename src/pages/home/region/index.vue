4eecb0520b794eb894eab51c982bdbcc12d2d6c5
hewenyu <he875027547@126.com>
2023-08-23 00:03:19 +0800

    完成地区数据接入

--- src/pages/home/region/index.vue
+++ src/pages/home/region/index.vue
@@ -2,26 +2,71 @@
   <div class="region">
     <span>地区:</span>
     <ul>
-      <li class="active">全部</li>
+      <li :class="{ active: currentRegion == '' }" @click="changeRegion('')">
+        全部
+      </li>
+      <li
+        v-for="region in regionList"
+        :key="region.id"
+        :class="{ active: currentRegion == region.id }"
+        @click="changeRegion(region.id)"
+      >
+        {{ region.name }}
+      </li>
       <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li>杭州</li>
-      <li v-for="i in 20" :key="i">杭州1</li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+// 导入request函数
+import { reqHospitalLevelOrRegion, DICT_CODE } from "@/api/home/index";
+// 导入接口数据对象
+import {
+  HospitalLevelOrRegionData,
+  HospitalLevelOrRegionArr,
+} from "@/api/home/type";
+
+// 导入组合式api
+import { ref, onMounted } from "vue";
+
+// 当前选中的地区
+let currentRegion = ref<string>("");
+
+// 地区数据列表
+let regionList = ref<HospitalLevelOrRegionArr>([]);
+
+function changeRegion(region: string) {
+  console.log("changeRegion()", region);
+  currentRegion.value = region;
+  // todo 联动地区修改
+}
+
+onMounted(() => {
+  requestRegion();
+});
+
+/**
+ * 请求地区
+ */
+const requestRegion = async () => {
+  const result: HospitalLevelOrRegionData = await reqHospitalLevelOrRegion(
+    DICT_CODE.REGIONO
+  );
+
+  console.log("------>requestRegion():", result);
+  // 更新响应式数据
+  regionList.value = result.data;
+};
 </script>
+
+<script lang="ts">
+// vue2 写法
+export default {
+  name: "Region",
+};
+</script>
+
 
 <style scoped lang="scss">
 .region {
@@ -40,7 +85,7 @@
   margin-bottom: 10px;
 }
 .region span {
-  width: 60px;
+  width: 40px;
 }
 
 .active {
